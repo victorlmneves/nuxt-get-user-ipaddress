ab8fdbdec587813325d95ab28a43191ec10a5a92
Victor Neves <info@victorneves.dev>
2024-08-12 15:30:29 +0100

    Restore setHeader for X-Frame-Options

--- src/server/middleware/x-frame-options.ts
+++ src/server/middleware/x-frame-options.ts
@@ -1,5 +1,5 @@
 import { type H3Event, defineEventHandler } from 'h3';
 
 export default defineEventHandler(async (event: H3Event) => {
-    // event.node.res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    event.node.res.setHeader('X-Frame-Options', 'SAMEORIGIN');
 });
